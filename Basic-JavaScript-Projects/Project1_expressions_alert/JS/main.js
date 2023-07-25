4ed0e1cb567bc4b6369bc559f83ef1ce6ce1a4bf
Zhaoweizoe <ukzhaowei@outlook.com>
2023-07-25 18:16:10 +0100

    Basic JavaScript Projects Assignment

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello,World!");//first window alert
+
+document.write("Hello,World!<br>");//document.write() Method
+
+var A="This is a string";
+document.write(A);//Variable Assignment
+
+var B="This is the second alert";
+window.alert(B);//Variables
+
+document.write("Lisa told Bart,\"Knock it off,Bart！Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart Responded.<br>");//Escape Character Assignment
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr.Seuss"+ "<br>");//Concatenating a String
+
+var C="Good"+"afternoon"
+document.write(C+ "<br>")//Concatenated String
+
+var Family="The Green",Dad="Bob",Mom="Lily",Daughter="Lucy",Son="Jack";
+document.write(Dad+ "<br>");//Multiple Variables Assignment
+
+document.write(3+8+ "<br>")//Expression Assignment
+
+var Sent1="This is the beginning of the string";
+var Sent2="and this is the end of the string";
+document.write(Sent1+Sent2);//Variables+Concatenation
