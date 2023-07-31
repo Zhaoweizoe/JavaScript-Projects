33417b11b7df50157ebd6f7c42517d63b4c547b5
Zhaoweizoe <ukzhaowei@outlook.com>
2023-07-31 16:58:23 +0100

    Project5_Comparisons_Type_Coercion Assignment

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -15,6 +15,29 @@ document.write("<br>"); // Inserting a line break
 document.write(2==3);//Double Equal Signs Assignment
 document.write("<br>"); // Inserting a line break
 document.write(3==3);//Double Equal Signs Assignment
+document.write("<br>"); // Inserting a line break
+X=30;
+Y=30;
+document.write(X===Y);//Return true by ensuring to match the data type and value
+document.write("<br>"); // Inserting a line break
+X=30;
+Y="dog";
+document.write(X===Y);//Return false by writing a different data type and different value
+document.write("<br>"); // Inserting a line break
+X=30;
+Y="30";
+document.write(X===Y);//Return false by writing a different data type but the same value for both
+document.write("<br>"); // Inserting a line break
+X=30;
+Y=60;
+document.write(X===Y);//Return false by writing the same data type but a different value for both.
+document.write("<br>"); // Inserting a line break
+document.write(3>1&&8>6);//utilize the AND operator to display true and false
+document.write(3<1&&8>6);//utilize the AND operator to display true and false
+document.write(3>1||8<6);//Utilize the OR operator to display true and false
+document.write(3<1||8<6);//Utilize the OR operator to display true and false
+document.write("<br>"); // Inserting a line break
+document.write("cat"+2);//an expression combining a string and a number
 
 function my_Function(){
     document.getElementById("Test").innerHTML= 0/0;
@@ -38,6 +61,19 @@ document.getElementById("infinity").innerHTML=1/0;
 function secondInfinityAssignment(){
 document.getElementById("negativeInfinity").innerHTML= -1/0;
 }//Infinity Assignment
+
+function not_Function(){
+    document.getElementById("Not").innerHTML=!(5>3);
+}//utilize the NOT operator to display true and false
+
+function not_2Function(){
+    document.getElementById("Not2").innerHTML=!(5<3);
+}//utilize the NOT operator to display true and false
+
+
+
+
+
 
 
 
